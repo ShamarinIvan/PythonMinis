8dfb032b0d320fe6a7a7ba500e2952fd7b16c8b7
Shimney <87991667+ShamarinIvan@users.noreply.github.com>
2024-11-05 21:03:21 +0700

    Update 3 mini.py

--- 3 mini.py
+++ 3 mini.py
@@ -2,10 +2,7 @@ def solution(a):
     matrix = []
     t = a.split(" | ")
     for i in range(len(t)):
-        matrix.append(t[i].split())
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            matrix[i][j] = float(matrix[i][j])
+        matrix.append([float(el) for el in t[i].split()])
     return matrix
 if __name__ == "__main__":
     assert solution("1 2 3 | 4 5 6 | 7 8 9") == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
