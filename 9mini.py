90d372da0ab168a44ca03c4e15fb59b84c85eb4c
Shimney <87991667+ShamarinIvan@users.noreply.github.com>
2024-11-19 22:34:48 +0700

    Add files via upload

---
+++ 9mini.py
@@ -0,0 +1,37 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity=16):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+        
+    def get(self, key):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return None
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if(len(self.cache) > self.capacity):
+            self.cache.popitem(last = False)
+
+def fibonacci(n, cache = None):
+    if cache is None:
+        cache = LRUCache(10)
+    
+    if n <= 2:
+        return 1
+    if((result := cache.get(n)) is not None):
+        return result
+    result = fibonacci(n - 1, cache) + fibonacci(n - 2, cache)
+    cache.put(n, result)
+    return result
+
+print(fibonacci(6))
+        
+
+    
+    
