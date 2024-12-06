5d3ce2780d099006c5d4db7a481a535d03128471
Shimney <87991667+ShamarinIvan@users.noreply.github.com>
2024-12-07 01:23:57 +0700

    Update 6mini.py

--- 6mini.py
+++ 6mini.py
@@ -1,15 +1,12 @@
-def flatten(array, depth = 1):
-    ans = array
-    for i in range(depth):
-        ans1 = []
-        for el in ans:
-            if not isinstance(el, list):
-                ans1.append(el)
+def flatten(array, depth=None):
+    def _flatten(arr, current_depth):
+        for el in arr:
+            if isinstance(el, list) and (current_depth is None or current_depth > 0):
+                yield from _flatten(el, None if current_depth is None else current_depth - 1)
             else:
-                ans1.extend(el)
-        ans = ans1
-    return ans
+                yield el
+    return list(_flatten(array, depth))
 if __name__ == "__main__":
     assert(flatten([1,2,3,[4],5])) == [1,2,3,4,5]
     assert(flatten([1,2,[4,5,[6,7,8]],9,[10]], 2)) == [1,2,4,5,6,7,8,9,10]
-    assert(flatten([1,2,[4,5,[6,7,8]],9,[10]])) == [1,2,4,5,[6,7,8],9,10]
+    assert(flatten([1,2,[4,5,[6,7,8]],9,[10]])) == [1,2,4,5,6,7,8,9,10]
