47af6565e7ef561845973b3ec48562e9ee6f1e4a
Shimney <87991667+ShamarinIvan@users.noreply.github.com>
2024-12-07 00:38:12 +0700

    Update 4 mini.py

--- 4 mini.py
+++ 4 mini.py
@@ -1,3 +1,4 @@
+import unittest
 def reverse_dict(d):
     reversed_dict = {}
     for key, value in d.items():
@@ -10,7 +11,16 @@ def reverse_dict(d):
             reversed_dict[value] = key
     return reversed_dict
 reversed_dict = reverse_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832})
+class MyTestCase(unittest.TestCase):
+    def test1(self):
+        with self.assertRaises(TypeError):
+            reverse_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}) == {97832: ("Ivanov", "Kuznecov"), 55521: "Petrov"}
+    def test2(self):
+        with self.assertRaises(TypeError):
+            reverse_dict({"Ivanov": 97832, "Petrov": 97832, "Kuznecov": 97832}) == {97832: ("Ivanov", "Petrov", "Kuznecov")}
+    def test3(self):
+        with self.assertRaises(TypeError):
+            reverse_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": [97832, 55521]})
+    
 if __name__ == "__main__":
-    assert reverse_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}) == {97832: ("Ivanov", "Kuznecov"), 55521: "Petrov"}
-    assert reverse_dict({"Ivanov": 97832, "Petrov": 97832, "Kuznecov": 97832}) == {97832: ("Ivanov", "Petrov", "Kuznecov")}
-    assert reverse_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": [97832, 55521]})
+    unittest.main()
