6771e04f53a533f7f49a3f3ebd4d843c71a85b33
Shimney <87991667+ShamarinIvan@users.noreply.github.com>
2024-12-07 11:58:23 +0700

    Update 12_mini.py

--- 12_mini.py
+++ 12_mini.py
@@ -1,10 +1,11 @@
+from functools import wraps
 def coroutine(func):
-	def wrapper(*args, **kwargs):
-		result = func(*args, **kwargs)
-		next(result)
-		return result
-
-	return wrapper
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        next(result)
+        return result
+    return wrapper
 
 @coroutine
 def storage():
@@ -27,19 +28,19 @@ def storage1():
         if not was_there:
             values.add(val)
 def test():
-	try:
-		norm_storage = storage()
-		print(norm_storage.send(42))
-		print(norm_storage.send(42))
-	except TypeError:
-		print("Doesnt work(")
-	finally:
-		print("it works)")
-	try:
-		silly_storage = storage1()
-		print(silly_storage.send(42))
-		print(silly_storage.send(42))
-	except TypeError:
-		print("doesnt work(")
+    try:
+        norm_storage = storage()
+        print(norm_storage.send(42))
+        print(norm_storage.send(42))
+    except TypeError:
+        print("Doesnt work(")
+    finally:
+        print("it works)")
+    try:
+        silly_storage = storage1()
+        print(silly_storage.send(42))
+        print(silly_storage.send(42))
+    except TypeError:
+        print("doesnt work(")
 if __name__ == '__main__':
-	test()
+    test()
